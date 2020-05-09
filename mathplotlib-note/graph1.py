bf6f0442c2745c09abcc7be36bfa8949a964cd19
Chunjeho <jejehoho3317@gmail.com>
2020-05-09 23:38:21 +0900

    Update graph1.py

--- mathplotlib-note/graph1.py
+++ mathplotlib-note/graph1.py
@@ -1,14 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(-20, 20, num = 500)
-x1 = np.linspace(-20, 20, num = 500)
+x = np.linspace(-20, 20, num = 10000)
+x1 = np.linspace(-20, 20, num = 10000)
 
-y = np.zeros((500), dtype = 'float64')
-y = x/(x**2+1)
+y = np.zeros((10000), dtype = 'float64')
+y = x/((x**2)+1)
 
-y1 = np.zeros((500), dtype = 'float64')
+y1 = np.zeros((10000), dtype = 'float64')
 y1 = x*0.0
 
-plt.plot(x,y,'g',x1,y1,'r')
+dx = np.diff(x)
+dy = np.diff(y)
+fp = np.divide(dy, dx)
+
+plt.plot(x[0:9999],y[0:9999],'g',x1,y1,'k',x[0:9999],fp,'r')
 plt.show()
