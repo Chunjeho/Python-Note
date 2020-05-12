21facc93240b4d0c7b4f10a87937073023f326ac
Chunjeho <jejehoho3317@gmail.com>
2020-05-12 21:18:09 +0900

    Add files via upload
    Coincidence graph

---
+++ matplotlib.pyplot-note/HeartBit.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+with plt.xkcd():
+    _x = [c for c in range(1,100)]
+    xs = np.array(_x)
+    ys = (((-1)**(xs-1))*(2**xs))/(xs*np.log(2))
+
+    plt.title("Heart-Bit")
+    plt.plot(xs,ys, color="blue")
+    plt.savefig('heart-bit.png')
+    plt.show()
