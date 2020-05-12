98c2a6b12452bfed2a29fef08b55d61552e45855
Chunjeho <jejehoho3317@gmail.com>
2020-05-12 16:05:04 +0900

    Add files via upload

--- matplotlib.pyplot-note/Graph1.py
+++ matplotlib.pyplot-note/Graph1.py
@@ -1,18 +1,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(-20, 20, num = 10000)
-x1 = np.linspace(-20, 20, num = 10000)
+with plt.xkcd():
+    x = np.linspace(-20, 20, num = 10000)
+    x1 = np.linspace(-20, 20, num = 10000)
 
-y = np.zeros((10000), dtype = 'float64')
-y = x/((x**2)+1)
+    y = np.zeros((10000), dtype = 'float64')
+    y = x/((x**2)+1)
 
-y1 = np.zeros((10000), dtype = 'float64')
-y1 = x*0.0
+    y1 = np.zeros((10000), dtype = 'float64')
+    y1 = x*0.0
 
-dx = np.diff(x)
-dy = np.diff(y)
-fp = np.divide(dy, dx)
+    dx = np.diff(x)
+    dy = np.diff(y)
+    fp = np.divide(dy, dx)
 
-plt.plot(x[0:9999],y[0:9999],'g',x1,y1,'k',x[0:9999],fp,'r')
-plt.show()
+    plt.plot(x1,y1,'k', label='f(x) = 0')
+    plt.plot(x[0:9999],y[0:9999],'g', label="f(x)")
+    plt.plot(x[0:9999],fp,'r', label="f'(x)")
+
+    plt.title("x/(x^2+1)")
+    plt.xlabel("x")
+    plt.ylabel("f(x)")
+
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig('Graph1.png')
+    plt.show()
