75fe829d8e5b923864a263d860079811bd47f668
Chunjeho <jejehoho3317@gmail.com>
2020-07-30 21:54:17 +0900

    print university info, i.e. Uni name, computer science course, link

---
+++ UK Colleges Computer Science Curriculum (Undergraduate)/ComputerScience.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+
+def UniTables():
+    result = requests.get("https://www.theguardian.com/education/ng-interactive/2019/jun/07/university-guide-2020-league-table-for-computer-science-information")
+
+    soup = BeautifulSoup(result.text, "html.parser")
+    tables = soup.find("tbody")
+
+    return tables
+
+def UniInfo():
+
+    tables = UniTables()
+    uniNames = []
+    uniNameRows = tables.find_all("tr", {"class":"c-table__row"})
+    uniLinkRows = tables.find_all("tr", {"class":"c-table__row--course-list"})
+
+    cs = []
+
+    _curriculum = []
+
+    for uniNameRow in uniNameRows:
+        uniName = uniNameRow.find("td", {"class":"c-table__data--name"})
+
+        if uniName is not None:
+            uniName = uniName.find("a").string
+            uniNames.append(uniName)
+
+        else:
+            continue
+
+    for i in range(len(uniNames)):
+        links = uniLinkRows[i].find_all("a", {"class":"c-course-list__course-link"})
+        
+        for link in links:
+            cs.append({"University":uniNames[i], "Course":link.get_text(), "link":link["href"]})
+
+        _curriculum.append(cs)
+    
+    return _curriculum
---
+++ UK Colleges Computer Science Curriculum (Undergraduate)/main.py
@@ -0,0 +1,5 @@
+from ComputerScience import UniInfo
+
+curriculum = UniInfo()
+
+print(len(curriculum))
